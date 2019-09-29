6128fc4ccdffb8839e168f08a44bf3609a0610bd
endo1234 <51956469+endo1234@users.noreply.github.com>
2019-09-29 19:17:51 +0900

    create index.html and main.js

---
+++ main.js
@@ -0,0 +1,23 @@
+const lastName = '山田';
+const firstName = '太郎';
+const fullName = lastName + ' ' + firstName;
+const myAge = 10;
+const friendAge = 13;
+const totalAge = myAge + friendAge;
+const numberOfDays = totalAge * 365;
+const numberOfChildren = 3;
+const numberOfCake = 10;
+const numberOfCakesByChild = numberOfCake / numberOfChildren;
+const numberOfRestOfCakes = numberOfCake % numberOfChildren;
+
+console.log(lastName);
+console.log(firstName);
+console.log(fullName);
+console.log(myAge);
+console.log(friendAge);
+console.log(totalAge);
+console.log(numberOfDays);
+console.log(numberOfChildren);
+console.log(numberOfCake);
+console.log(numberOfCakesByChild);
+console.log(numberOfRestOfCakes);
